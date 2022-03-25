939a1a72b2579d44a99e836c6243c4d4c51980e7
Tony Jin <yuhueijin@gmail.com>
2022-03-25 23:02:16 +0800

    0033 if else

--- 0033.SearchInRotatedSortedArray/Program.cs
+++ 0033.SearchInRotatedSortedArray/Program.cs
@@ -1,2 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+var nums = new int[] { 3, 1 };
+var target = 1;
+var solution = new Solution();
+var result = solution.Search(nums, target);
+Console.WriteLine(result);
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        if (nums.Length == 1)
+        {
+            if (target == nums[0])
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (target < nums[i])
+            {
+                break;
+            }
+            if (target == nums[i])
+            {
+                return i;
+            }
+            if (target == nums[i + 1])
+            {
+                return i + 1;
+            }
+            if (nums[i] > nums[i + 1])
+            {
+                break;
+            }
+        }
+
+        for (int i = nums.Length - 1; i > 0; i--)
+        {
+            if (target > nums[i])
+            {
+                break;
+            }
+            if (target == nums[i])
+            {
+                return i;
+            }
+            if (target == nums[i - 1])
+            {
+                return i - 1;
+            }
+            if (nums[i] < nums[i - 1])
+            {
+                break;
+            }
+
+        }
+        return -1;
+    }
+}
