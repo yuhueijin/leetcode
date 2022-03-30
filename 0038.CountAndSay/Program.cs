bd99d93268c5b255e6d0467b1365c9b00305241f
Tony Jin <yuhueijin@gmail.com>
2022-03-30 21:24:35 +0800

    0038 My version

--- 0038.CountAndSay/Program.cs
+++ 0038.CountAndSay/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+var n = 4;
+var solution = new Solution();
+var result = solution.CountAndSay(n);
+Console.WriteLine(result);
+
+public class Solution
+{
+    public string CountAndSay(int n)
+    {
+        var result = "1";
+        for (int i = 1; i < n; i++)
+        {
+            result = CountAndSay(result);
+        }
+        return result;
+    }
+    private string CountAndSay(string input)
+    {
+        var result = string.Empty;
+        var element = input[0];
+        var count = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == element)
+            {
+                count++;
+            }
+            else
+            {
+                result += $"{count}{element}";
+                count = 1;
+                element = input[i];
+            }
+        }
+        if (count != 0)
+        {
+            result += $"{count}{element}";
+        }
+        return result;
+    }
+}
