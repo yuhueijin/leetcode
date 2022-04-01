f212bcdeb548e7ff5e9d5468ecf0d4122e4780dc
Tony Jin <yuhueijin@gmail.com>
2022-04-02 00:02:04 +0800

    0048 Rotate Groups of Four Cells

--- 0048.RotateImage/Program.cs
+++ 0048.RotateImage/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 var matrix = new int[3][];
-matrix[0] = new int[] { 1, 2, 3, 4 };
-matrix[1] = new int[] { 5, 6, 7, 8 };
-matrix[2] = new int[] { 9, 10, 11, 12 };
+matrix[0] = new int[] { 1, 2, 3 };
+matrix[1] = new int[] { 4, 5, 6 };
+matrix[2] = new int[] { 7, 8, 9 };
 var solution = new Solution();
 solution.Rotate(matrix);
 
@@ -16,36 +16,16 @@ public class Solution
 {
     public void Rotate(int[][] matrix)
     {
-        var set = new HashSet<string>();
         var n = matrix.Length;
-        for (int y = 0; y < n; y++)
+        for (int y = 0; y < (n + 1) / 2; y++)
         {
-            for (int x = 0; x < n; x++)
+            for (int x = 0; x < n / 2; x++)
             {
-                if (set.Contains($"{x},{y}"))
-                {
-                    continue;
-                }
-
-                var sourceX = x;
-                var sourceY = y;
-                var targetX = x + 1;
-                var targetY = y + 1;
-                var sourceElement = matrix[y][x];
-                var targetElement = 0;
-                var count = 0;
-                while (count <= 4)
-                {
-                    targetX = n - 1 - sourceY;
-                    targetY = sourceX;
-                    targetElement = matrix[targetY][targetX];
-                    matrix[targetY][targetX] = sourceElement;
-                    sourceElement = targetElement;
-                    sourceX = targetX;
-                    sourceY = targetY;
-                    count++;
-                    set.Add($"{targetX},{targetY}");
-                }
+                var temp = matrix[n - 1 - x][y];
+                matrix[n - 1 - x][y] = matrix[n - 1 - y][n - 1 - x];
+                matrix[n - 1 - y][n - 1 - x] = matrix[x][n - 1 - y];
+                matrix[x][n - 1 - y] = matrix[y][x];
+                matrix[y][x] = temp;
             }
         }
     }
