4c6c4d88344297775b49ef25b1fd9cac1e4b2afb
Tony Jin <yuhueijin@gmail.com>
2022-04-06 21:56:00 +0800

    0049 my version with dictionary

--- 0049.GroupAnagrams/Program.cs
+++ 0049.GroupAnagrams/Program.cs
@@ -9,6 +9,26 @@ public class Solution
 {
     public IList<IList<string>> GroupAnagrams(string[] strs)
     {
-
+        var result = new List<IList<string>>();
+        var dict = new Dictionary<string, List<string>>();
+        foreach (var item in strs)
+        {
+            var charArray = item.ToArray();
+            Array.Sort(charArray);
+            var target = new string(charArray);
+            if (dict.ContainsKey(target))
+            {
+                dict[target].Add(item);
+            }
+            else
+            {
+                dict.Add(target, new List<string>() { item });
+            }
+        }
+        foreach (var item in dict)
+        {
+            result.Add(item.Value);
+        }
+        return result;
     }
 }
