94f73b4d9a7c872942f7109f925e85a08c98b3d1
Tony Jin <yuhueijin@gmail.com>
2022-04-06 21:29:05 +0800

    0049 first commit

--- 0049.GroupAnagrams/Program.cs
+++ 0049.GroupAnagrams/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+var strs = new string[] { };
+var solution = new Solution();
+var result = solution.GroupAnagrams(strs);
+
+
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+
+    }
+}
